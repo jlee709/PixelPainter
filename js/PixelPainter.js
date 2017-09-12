801a89a989535e3eded65bfcaa80181555ceab99
Justin Lee <leejwg.3@gmail.com>
2017-09-11 18:19:12 -1000

    macro pseduo steps added

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -18,11 +18,76 @@ console.log('virgi is here');
 // of your html markup using the DOM API.
 
 
-
+// constraints --> can not manipulate html !!!!!
 
 
 
 // --! pseudo code for what needs to be completed !-- //
+
+
+
+
+
+// step one - Create the grid 
+
+	
+
+
+// step 2 - create the color grad (without color)
+
+
+
+
+
+// step 3 - make Erase and Clear buttins 
+
+
+
+
+// step 4 - make main grid clickable 
+
+
+
+
+
+// step 5 - make color grid clickable 
+
+
+
+
+
+// step 6 - add action fucntion to each grid Key 
+
+
+
+
+
+
+// step 7 - action for erase button / clear grid 
+
+
+
+
+
+// step 8 - link colors to color grid 
+
+
+
+
+
+
+// step 9 - clear grid (button)
+
+
+
+
+
+// step 10. link the color grid to mous click to write to the main grid 
+
+
+
+
+
 
 
 
