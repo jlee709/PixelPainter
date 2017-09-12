5c4792339f19a04ab39f492a776e3f71586ac1d2
Justin Lee <leejwg.3@gmail.com>
2017-09-11 16:58:38 -1000

    added spec in js file for ease of reading

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,3 +2,18 @@
 console.log("im here");
 console.log('Justin Is here');
 console.log('virgi is here');
+
+// specs For Pixel Painter
+
+// When instatiated it appends a 
+// new PixelPainter object to a HTML div 
+// element with an id of pp-canvas.
+// Module or Classical OOP Patterns for this.
+
+
+// The height and width attributes set the size of the canvas. 
+// must be a square 
+
+// Do not edit the index.html file. You will generate all 
+// of your html markup using the DOM API.
+
