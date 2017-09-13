27249067e2cd239cafc1b7cdebc7a7dfce144757
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-09-12 14:46:57 -1000

    filled cells with background color

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -29,7 +29,7 @@ console.log('virgi is here');
 // step one - Create the grid --- Can bind click function built into for loop
 function drawGrid(width, height, attributename, attribute){
 	var MainTableWrapper = document.createElement('div');
-	
+
 
 	for(var i = 1; i< height; i++){
 		var newTableCol = document.createElement('div');
@@ -41,7 +41,12 @@ function drawGrid(width, height, attributename, attribute){
 		 	var newTableRow = document.createElement('div');
 	 		newTableRow.setAttribute('data', j);
 	 		newTableRow.setAttribute('class', 'newTableRow');
+	 		newTableRow.addEventListener('click', function(event){
+			var thisElem = event.target;
+			thisElem.style = 'background-color: black';
+	}, true);
 	 		newTableCol.appendChild(newTableRow);
+
 
 	  }
 }
@@ -55,21 +60,28 @@ var newGrid = drawGrid(10,10, 'class', 'tableCol');
 var parent = document.getElementById('pixelPainter');
 		parent.appendChild(newGrid);
 // step 2 - create the color grid (without color)
-	
+
 	var newColorGrid = drawGrid(3,3, 'class', 'colorTable');
 		var parent = document.getElementById('pixelPainter');
 			parent.appendChild(newColorGrid);
 
 // step 3 - make Erase and Clear buttins
+var erButtonDiv = document.createElement('div');
+var eraseButton = document.createElement('button');
+eraseButton.setAttribute('id', 'eraseButton');
+erButtonDiv.appendChild(eraseButton);
+parent.appendChild(erButtonDiv);
+
+
 
 
 
 
 // step 4 - make main grid clickable
 
-// var clicker = function('click',function(){
 
-// };
+
+
 
 
 // step 5 - make color grid clickable
